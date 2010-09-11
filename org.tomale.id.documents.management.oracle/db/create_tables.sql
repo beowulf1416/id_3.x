4192188e735ed8b664136993400c6ece81552a0f
beowulf1416 <beowulf1416@gmail.com>
2010-09-11 14:39:14 +0800

    modified oracle create tables query

--- org.tomale.id.documents.management.oracle/db/create_tables.sql
+++ org.tomale.id.documents.management.oracle/db/create_tables.sql
@@ -2,30 +2,32 @@
  * document management tables
  */
 
-delimiter //
+/* modify below line to use a different schema */
+alter session set current_schema = documents;
 
 create sequence seq_doc_id;
 
 create table content_types (
-	id int not null comment 'content type id',
-	mime varchar2(200) not null
-)
-default character set utf8;
-//
+	id int not null,
+	mime varchar2(200) not null,
+	primary key (id)
+);
+
+comment on column content_types.id is 'pk, content type id';
+comment on column content_types.mime is 'mime type';
 
 create table documents (
-	id int not null comment 'pk, doc id',
+	id int not null,
 	content_type_id int not null,
 	path varchar2(500) not null,
 	filename varchar2(100) not null,
 	content blob not null,
 	attribs varchar2(200),
 	primary key (id),
-	foreign key (content_type_id) references content_types(id) on delete restrict,
-	index (path),
-	index (filename)
-)
-default character set utf8;
-//
+	foreign key (content_type_id) references content_types(id)
+);
 
-delimiter ;
+create index idx_doc_path on documents(path);
+create index idx_doc_fn on documents(filename);
+
+comment on column documents.id is 'pk, document id';
