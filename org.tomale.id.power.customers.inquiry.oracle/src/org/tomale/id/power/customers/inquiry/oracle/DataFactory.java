4c53a2fa9f018eb9f93cbeaecd75e18f72a3f53b
beowulf <beowulf1416@gmail.com>
2010-09-17 14:26:20 +0800

    added oracle power customers inquiry data provider

---
+++ org.tomale.id.power.customers.inquiry.oracle/src/org/tomale/id/power/customers/inquiry/oracle/DataFactory.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.tomale.id.power.customers.inquiry.oracle;
+
+import org.tomale.id.power.customers.inquiry.db.ICustomerInquiryData;
+import org.tomale.id.power.customers.inquiry.db.ICustomerInquiryDataFactory;
+
+/**
+ * @author ftomale
+ *
+ */
+public class DataFactory implements ICustomerInquiryDataFactory {
+
+	@Override
+	public ICustomerInquiryData getDataProvider() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
