44d7dc78b5abfbb82054ade5eb513f2d0256b0f9
beowulf <beowulf1416@gmail.com>
2010-09-20 10:35:42 +0800

    initial commit of project management code

---
+++ org.tomale.id.projects.management/src/org/tomale/id/projects/management/editors/ProjectEditorGeneralPage.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.tomale.id.projects.management.editors;
+
+import org.eclipse.ui.forms.editor.FormPage;
+
+/**
+ * @author ftomale
+ *
+ */
+public class ProjectEditorGeneralPage extends FormPage {
+
+	private static final String PAGE_ID = "org.tomale.id.projects.management.editors.page.general";
+	private static final String PAGE_TITLE = "General";
+	
+	public ProjectEditorGeneralPage(){
+		super(PAGE_ID, PAGE_TITLE);
+	}
+	
+}
