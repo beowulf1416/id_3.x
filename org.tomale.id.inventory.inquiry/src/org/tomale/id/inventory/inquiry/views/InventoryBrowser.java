704d113891ed06a19cd7424ba161162f4cf9c104
beowulf <beowulf1416@gmail.com>
2010-09-27 13:55:30 +0800

    added inventory inquiry project

---
+++ org.tomale.id.inventory.inquiry/src/org/tomale/id/inventory/inquiry/views/InventoryBrowser.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.tomale.id.inventory.inquiry.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * @author ftomale
+ *
+ */
+public class InventoryBrowser extends ViewPart {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+	 */
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
